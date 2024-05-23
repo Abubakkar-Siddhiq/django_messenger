5b052aab0696e3f8f626d83b9ef35c6b234e16d6
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2024-05-23 22:42:17 +0530

    Removed id field from FriendsSerializer

--- backend/chat/serializers.py
+++ backend/chat/serializers.py
@@ -7,22 +7,16 @@ class MessageSerializer(serializers.ModelSerializer):
         model = Message
         fields = '__all__'
 
-
 class ChatRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = ChatRoom
         fields = '__all__'
 
-# class MessageThreadSerializer(serializers.ModelField):
-#     class Meta:
-#         model = Message
-#         fields = '__all__'
-
 class FriendsSerializer(serializers.ModelSerializer):
     friends = serializers.SerializerMethodField()
     class Meta:
         model = User
-        fields = ['id', 'username', 'friends']
+        fields = ['username', 'friends']
 
     def get_friends(self, obj):
             return obj.friends.values_list('username', flat=True)
