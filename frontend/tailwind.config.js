1746b3947c1a269542b49a68f1abd252bd991b38
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2024-05-23 22:45:35 +0530

    edited config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,16 @@ export default {
       flex: {
         '2': '2 2 0%'
       },
+      keyframes: {
+        loadingFade: {
+          '0%': { opacity: '0' },
+          '50%': { opacity: '0.8' },
+          '100%': { opacity: '0' },
+        },
+      },
+      animation: {
+        loadingFade: 'loadingFade 1s infinite',
+      },
     },
   },
   plugins: [],
