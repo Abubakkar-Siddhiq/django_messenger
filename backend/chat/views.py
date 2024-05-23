663382578467437e2495a6a5257dfa71416e3fe8
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2024-05-23 22:43:46 +0530

    Added Mark as Read, List Friends logic

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -1,10 +1,12 @@
+from django.db.models import Q, F, Max
+from django.db.models.functions import Coalesce
 from rest_framework import generics
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import NotFound, PermissionDenied
-
+from django.shortcuts import get_object_or_404
 from .models import *
 from .serializers import *
 from user.serializers import UserProfileSerializer
@@ -13,26 +15,16 @@ from user.serializers import UserProfileSerializer
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def ChatView(request, room_id):
-    chatroom = ChatRoom.objects.filter(id=room_id).first()
-    messages = Message.objects.filter(room=room_id)
-    serializer = MessageSerializer(messages, many=True)
-    other_user = None
-
-    if not chatroom:
-        raise NotFound('Chat room not found')
-
-    if request.user.is_authenticated:
-        if request.user in chatroom.members.all():
-            for member in chatroom.members.all():
-                if member != request.user:
-                    other_user = member
-                    break
-            return Response(serializer.data)  # Return the response here
-        else:
-            raise PermissionDenied('You dont have permission to this')  
-    else:
-        raise PermissionDenied('You dont have permission to this')  
+    chatroom = get_object_or_404(ChatRoom, id=room_id)
     
+    if request.user not in chatroom.members.all():
+        raise PermissionDenied('You don\'t have permission to access this chat room')
+    
+    Message.objects.filter(receiver=request.user, room=chatroom, is_read=False).update(is_read=True)
+    messages = Message.objects.filter(room=chatroom)
+    serializer = MessageSerializer(messages, many=True)
+    
+    return Response(serializer.data)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -48,7 +40,7 @@ def GetOrCreateChatroom(request, username):
     my_chatrooms = request.user.chat_rooms.all()
     for chatroom in my_chatrooms:
         if other_user in chatroom.members.all():
-            members_data = UserProfileSerializer(chatroom.members.all(), many=True).data
+            members_data = UserProfileSerializer(chatroom.members.all(), many=True, context={'request': request}).data
             return Response({'chatroom': chatroom.id, 'members': members_data})
     
     # Create a new chatroom if none exists
@@ -56,7 +48,7 @@ def GetOrCreateChatroom(request, username):
     user = User.objects.get(username=username)
     chatroom.members.add(other_user, request.user)
     user.friends.add(other_user, request.user)
-    members_data = UserProfileSerializer(chatroom.members.all(), many=True).data
+    members_data = UserProfileSerializer(chatroom.members.all(), many=True, context={'request': request}).data
     return Response({'chatroom': chatroom.id, 'members': members_data})
 
 @api_view(['GET'])
@@ -65,3 +57,32 @@ def ListFriends(request):
     user = request.user
     serializer = FriendsSerializer(user)
     return Response(serializer.data)
+
+class ListFriendsProfile(generics.ListAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        current_user = self.request.user
+        return current_user.friends.annotate(
+            last_message_date = Coalesce(
+                Max('sent_message__created_at', filter=Q(sent_message__receiver=current_user)),
+                Max('received_message__created_at', filter=Q(received_message__sender=current_user))
+            )).order_by('-last_message_date')
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True, context={'request': request})
+        return Response(serializer.data)
+    
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def MarkAsRead(request):
+    msg_id = request.data.get('msg_id')
+    try:
+        message = Message.objects.get(id=msg_id)
+        message.is_read = True
+        message.save()
+        return Response({'status': 'success', 'message': 'Message marked as read'}, status=status.HTTP_200_OK)
+    except Message.DoesNotExist:
+        return Response({'status': 'error', 'message': 'Message not found'}, status=status.HTTP_404_NOT_FOUND)
