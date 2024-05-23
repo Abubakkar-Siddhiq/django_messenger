fdd65b77f5c703a2f474be0a2b8c2b0b6080f31f
Abubakkar-Siddhiq <abubakkarsiddhiq2k4@gmail.com>
2024-05-23 22:52:43 +0530

    Set Access Token time to 20 Minutes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,7 +62,7 @@ CHANNEL_LAYERS = {
 AUTH_USER_MODEL = "user.User"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=2),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
